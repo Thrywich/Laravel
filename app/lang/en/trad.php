b68a9a7f5b0d4090b4a7aed047f130e02f40b091
Aurel T <aurel.thry@gmail.com>
2024-02-08 17:16:57 +0100

    test deploiement auto

--- app/lang/en/trad.php
+++ app/lang/en/trad.php
@@ -24,7 +24,7 @@ return [
     'email' => 'E-mail',
 
     'password' => [
-        'add_passwd' => 'Add a new password',
+        'add_passwd' => 'Add a new passwordddddddddd',
         'see_passwd' => 'See all my passwords',
         'my_passwd' => 'My Passwords',
         'no_passwd' => 'You have no password yet.',
